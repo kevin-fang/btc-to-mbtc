f41c54712af128f22ad16b0323eefbbea840f8e4
Kevin Fang <kfang1233@gmail.com>
2018-02-26 22:49:13 -0500

    Fixed regular expression for price capturing

--- Source/content_script.js
+++ Source/content_script.js
@@ -8,13 +8,13 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var btcRegexp = /((\d+|\.)?(?:\.\d{1,2})?) (?:btc|BTC)/gi
+            var btcRegexp = /((?:\d+)|(?:\.\d+|\d+\.\d+)) (?:btc|BTC)/gi
             var match = btcRegexp.exec(text)
             //console.log(match)
             var replacedText = text.replace(btcRegexp, '$1');
 
             if (replacedText !== text) {
-            	console.log(match[1] * 1000)
+            	//console.log(match[1] * 1000)
             	replacedText = text.replace(btcRegexp, (match[1] * 1000 + ' mBTC'));
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
