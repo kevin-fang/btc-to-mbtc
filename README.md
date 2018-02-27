c18354b9b8ad6ba3c2df6538dc3aed9bf96b4fa4
Kevin Fang <kfang1233@gmail.com>
2018-02-26 22:36:45 -0500

    Initial commit with basic functionality

---
+++ README.md
@@ -0,0 +1,4 @@
+btc-to-mbtc
+=============
+
+Replaces all instances of BTC with their equivalent in mBTC, because BTC costs so much and tiny numbers representing huge amounts of USD is not the best way to represent Bitcoin.
---
+++ Source/content_script.js
@@ -0,0 +1,23 @@
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var btcRegexp = /((\d+|\.)?(?:\.\d{1,2})?) (?:btc|BTC)/gi
+            var match = btcRegexp.exec(text)
+            //console.log(match)
+            var replacedText = text.replace(btcRegexp, '$1');
+
+            if (replacedText !== text) {
+            	console.log(match[1] * 1000)
+            	replacedText = text.replace(btcRegexp, (match[1] * 1000 + ' mBTC'));
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
